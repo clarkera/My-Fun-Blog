40ab3a1e73a4a5876471f637bb1a0b1d5ae4c218
clarkera <rclarke9941@gmail.com>
2024-03-28 22:09:29 -0400

    updated code

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+
+
+
+
+<input type = "checkbox" id = "theme-switcher"></input>
+themeSwicther.addEventListener("click",function(){console.log('')})
+< themeSwicther.addEventListener("click", toggleTheme)>
